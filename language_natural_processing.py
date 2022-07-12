a0da0cb6e62e9969ed12075d14148037d9b1f23d
AdsonRoberto <adson.uece@gmail.com>
2022-07-12 09:22:17 -0300

    usando marcacao pos junto com os tokens

--- language_natural_processing.py
+++ language_natural_processing.py
@@ -7,3 +7,8 @@ spacy.__version__
 
 pln = spacy.load('pt')
 pln
+
+documento = pln('Estou aprendendo processamento de linguagem natural, curso em Curitiba')
+
+for token in documento:
+  print(token.text, token.pos_)
